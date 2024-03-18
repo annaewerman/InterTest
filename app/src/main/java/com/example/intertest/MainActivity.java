34fa95e2c0ab80633a11950cfe45b0dfa330e155
annaewerman <eko15aew@student.lu.se>
2024-03-18 12:59:43 +0100

    started

--- app/src/main/java/com/example/intertest/MainActivity.java
+++ app/src/main/java/com/example/intertest/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.intertest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button mybutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mybutton = findViewById(R.id.button2);
+
     }
 }
