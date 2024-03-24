d0c08236f3e2ae48b00aa5fa89460565ae530782
Nils Gustav Jakob Strässle <nils_gustav_jakob.strassle.8568@student.lu.se>
2024-03-24 19:13:24 +0100

    implement open interaction

--- app/src/main/java/com/example/intertest/open_activity.java
+++ app/src/main/java/com/example/intertest/open_activity.java
@@ -2,13 +2,49 @@ package com.example.intertest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class open_activity extends AppCompatActivity {
+public class open_activity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor gyroscope;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+
+        if(x < -3) {
+            Toast.makeText(getApplicationContext(), "Opened", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
