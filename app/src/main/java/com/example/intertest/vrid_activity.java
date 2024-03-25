f1982184acfb749a00a5e7b735e2068cc002d09d
St3fan123 <38635969+St3fan123@users.noreply.github.com>
2024-03-25 14:48:49 +0100

    Vrid funktionen två sätt

--- app/src/main/java/com/example/intertest/vrid_activity.java
+++ app/src/main/java/com/example/intertest/vrid_activity.java
@@ -3,19 +3,72 @@ package com.example.intertest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.View;
 
-public class vrid_activity extends AppCompatActivity {
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class vrid_activity extends AppCompatActivity implements SensorEventListener{
+
+
+    private SensorManager sensorManager;
+    private Sensor gyroscope;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vrid);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
     }
 
     public void launchHome(View v){
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
     }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float z = event.values[1];
+
+        float y = event.values[2];
+
+        if(z < -4 || z> 4) {
+            Toast.makeText(getApplicationContext(), "The turn of the screw", Toast.LENGTH_SHORT).show();
+
+        }
+        if(y < -3 || y> 3) {
+            Toast.makeText(getApplicationContext(), "Timber!", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+    }
 }
+
