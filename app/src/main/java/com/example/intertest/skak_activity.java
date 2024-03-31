4fa710367c68c64c8540352bf0127b68daaabfff
annaewerman <eko15aew@student.lu.se>
2024-03-31 14:18:40 +0200

    Fixed feedback

--- app/src/main/java/com/example/intertest/skak_activity.java
+++ app/src/main/java/com/example/intertest/skak_activity.java
@@ -41,8 +41,6 @@ public class skak_activity extends AppCompatActivity implements SensorEventListe
 
 
             if(acceleration > SHAKE_THRESHOLD){
-                View view = findViewById(R.id.activity_skak); // Replace main_layout with your root layout id
-                view.setBackgroundColor(Color.GREEN);
                 Toast.makeText(getApplicationContext(), "Shaken", Toast.LENGTH_SHORT).show();
             }
         }
